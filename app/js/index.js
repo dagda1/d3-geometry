52b50ebbfaf50dd5397c8dfd0ec27a887ce62af7
dagda1 <dagda1@scotalt.net>
2015-12-21 20:21:53 +0000

    use simple react router

--- app/js/index.js
+++ app/js/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
+import { createHistory } from 'history';
+import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
+import reducers from './reducers';
 
-import App from './components/app.js';
+import App from './components/app';
 
 require("../css/style.css");
 
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routeReducer
+}));
+const store = createStore(reducer);
+const history = createHistory();
+
+syncReduxAndRouter(history, store);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
+  </Provider>,
   document.querySelector('.container')
 );
