f957ae8a30bff0300b397c18f61a95a6d9e45cf2
dagda1 <dagda1@scotalt.net>
2016-03-13 07:36:51 +0000

    seed 2012-2013 season

--- migrations/20160212184004_results.js
+++ migrations/20160212184004_results.js
@@ -7,6 +7,7 @@ exports.up = function(knex, Promise) {
     table.string('result', 1).notNullable();
     table.integer('position').notNullable();
     table.integer('team_id').unsigned().index().references('id').inTable('teams');
+    table.string('location', 1).notNullable();
     table.integer('manager_id').unsigned().index().references('id').inTable('managers');
   });
 };
